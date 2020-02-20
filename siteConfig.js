c083f276b9aa1a97ada60e1943078659a418bd39
lordKnighton <keltheceo@gmail.com>
2020-02-19 17:31:52 -0800

    1st commit

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'London SNATCH',
+  shortName: 'London',
+  description: 'Kelvin Knighton\'s Portfolio',
+  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Imed Adel',
+  twitter: 'Imed_Adel'
+}
